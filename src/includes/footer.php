547762c0f24f0ff9dc90ec26043878c4d0f96f8c
ianbrown78 <ianbrown78@gmail.com>
2025-05-27 03:13:19 +0000

    Update footer style

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,8 +1,9 @@
 <?php
 // includes/footer.php
 ?>
-    </div> <footer class="bg-gray-800 text-white text-center p-4 mt-auto">
-        <p>&copy; <?php echo date("Y"); ?> Voting System. All rights reserved.</p>
+    </div>
+    <footer class="w-full h-64 bg-gray-800 static bottom-0 text-white text-center p-4">
+        <p>&copy; <?php echo date("Y"); ?> Flaxton Consulting. All rights reserved.</p>
     </footer>
 </body>
 </html>
