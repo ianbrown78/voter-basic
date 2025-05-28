80f0fd973172bcaf0c89e5812bd85023514c4fd6
Ian Brown <1063820+ianbrown78@users.noreply.github.com>
2025-05-28 15:47:09 +1000

    Submit causes a 500

--- src/admin/manage_users.php
+++ src/admin/manage_users.php
@@ -43,8 +43,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     $csv = fopen($_FILES['csv']['tmp_name'], 'r');
                     
                     // Cycle through each line of the sheet and insert the voters into the table
-                    $stmt = $pdo->prepare("INSERT INTO users (email, name) VALUES (?, ?)");
-                    $stmt->bind_param($email, $name);
+                    // $stmt = $pdo->prepare("INSERT INTO users (email, name) VALUES (?, ?)");
+                    // $stmt->bind_param($email, $name);
                     
                     // while(($getData = fgetcsv($csv, 100000, ",")) !== FALSE) {
                     //     if (count($getData) != 2) {
