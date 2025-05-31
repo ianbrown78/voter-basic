03fef6e5fdf2bc7dc4edfc7e9f72c3c38549d45d
Ian Brown <1063820+ianbrown78@users.noreply.github.com>
2025-05-31 17:57:56 +1000

    Add Results Link

--- src/includes/header.php
+++ src/includes/header.php
@@ -11,8 +11,6 @@ if (strpos($_SERVER['PHP_SELF'], '/admin/') !== false || strpos($_SERVER['PHP_SE
 }
 
 $active_page = end(explode('/', $_SERVER['SCRIPT_NAME']));
-echo "<script>console.log('Debug Path: " . $_SERVER['SCRIPT_NAME'] . "' );</script>";
-echo "<script>console.log('Debug Script: " . $active_page . "' );</script>";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,11 +35,12 @@ echo "<script>console.log('Debug Script: " . $active_page . "' );</script>";
                     </a>
                 <?php elseif (is_admin_logged_in() && strpos($_SERVER['PHP_SELF'], '/admin/') !== false ): ?>
                     <span class="mr-4">Welcome, <?php echo htmlspecialchars($_SESSION['first_name'] . ' ' . $_SESSION['last_name']); ?>!</span>
-                    <a href="index.php" class="<?php echo $active_page == 'manage_admins.php' ? 'hover:text-blue-200 active:text-blue-300 px-2 active' : 'hover:text-blue-200 px-2 active:text-blue-300'?>">Dashboard</a>
+                    <a href="index.php" class="<?php echo $active_page == 'index.php' ? 'hover:text-blue-200 active:text-blue-300 px-2 active' : 'hover:text-blue-200 px-2 active:text-blue-300'?>">Dashboard</a>
                     <a href="manage_admins.php" class="<?php echo $active_page == 'manage_admins.php' ? 'hover:text-blue-200 active:text-blue-300 px-2 active' : 'hover:text-blue-200 px-2 active:text-blue-300'?>">Admins</a>
-                    <a href="manage_elections.php" class="<?php echo $active_page == 'manage_admins.php' ? 'hover:text-blue-200 active:text-blue-300 px-2 active' : 'hover:text-blue-200 px-2 active:text-blue-300'?>">Elections</a>
-                    <a href="manage_candidates.php" class="<?php echo $active_page == 'manage_admins.php' ? 'hover:text-blue-200 active:text-blue-300 px-2 active' : 'hover:text-blue-200 px-2 active:text-blue-300'?>">Candidates</a>
-                    <a href="manage_users.php" class="<?php echo $active_page == 'manage_admins.php' ? 'hover:text-blue-200 active:text-blue-300 px-2 active' : 'hover:text-blue-200 px-2 active:text-blue-300'?>">Voters</a>
+                    <a href="manage_elections.php" class="<?php echo $active_page == 'manage_elections.php' ? 'hover:text-blue-200 active:text-blue-300 px-2 active' : 'hover:text-blue-200 px-2 active:text-blue-300'?>">Elections</a>
+                    <a href="manage_candidates.php" class="<?php echo $active_page == 'manage_candidates.php' ? 'hover:text-blue-200 active:text-blue-300 px-2 active' : 'hover:text-blue-200 px-2 active:text-blue-300'?>">Candidates</a>
+                    <a href="manage_users.php" class="<?php echo $active_page == 'manage_users.php' ? 'hover:text-blue-200 active:text-blue-300 px-2 active' : 'hover:text-blue-200 px-2 active:text-blue-300'?>">Voters</a>
+                    <a href="results.php" class="<?php echo $active_page == 'results.php' ? 'hover:text-blue-200 active:text-blue-300 px-2 active' : 'hover:text-blue-200 px-2 active:text-blue-300'?>">Results</a>
                     <a href="<?php echo $basePath; ?>admin/logout.php" class="button-icon-align bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                         <i class="bx bx-door-open"></i> Logout
                     </a>
